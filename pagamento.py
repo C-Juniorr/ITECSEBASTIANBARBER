16a1e1644d3fb19ce8ec5edadd165915c469620a
Clodoaldo Junior <156717109+C-Juniorr@users.noreply.github.com>
2025-03-10 16:50:10 -0300

    Update pagamento.py

--- pagamento.py
+++ pagamento.py
@@ -12,9 +12,11 @@ def gerarpagamento(nome,numero,valor,titulo,id):
     id = int(id)
     if id == 1:
         valor = 75
+        valor = 0.01
         titulo = "Plano Mensal Corte"
     elif id == 2:
         valor = 130
+        valor = 0.01
         titulo = "Plano Mensal Corte é Barba"
     else:
         valor = 10000
